880790e1ba652e46e96c9d84c2533204b5be4cad
IngridSR95 <sringrid95@gmail.com>
2024-05-23 22:14:43 -0300

    Tentativa 1 de Resolução, ainda com alguns erros

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // criação do objeto Scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+            System.out.println("Seja bem-vindo ao seu Banco!");
+            System.out.println("\nPor favor, digite o número da Conta: ");
+            int numeroConta = scanner.nextInt();
+
+            System.out.println("\nPor favor, digite o número da Agência: ");
+            String agencia = scanner.next();
+
+            System.out.println("\nPor favor, digite o nome do Usuário: ");
+            String nomeCliente = scanner.next();
+
+            Double saldoConta = 237.48;
+
+            System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldoConta + " já está disponível para saque!");
+
+            // Fechar o scanner para evitar vazamentos de recursos
+            scanner.close();
     }
 }
